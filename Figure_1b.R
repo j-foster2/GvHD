5de71519e5ab512fcef620be146befdef8a98e41
Joseph Foster <jfoster3@longleaf-login4.its.unc.edu>
2023-07-03 23:47:04 -0400

    Updated Figure 1b Analysis.

--- Figure_1b.R
+++ Figure_1b.R
@@ -37,3 +37,7 @@ rna_k4me3_data_plot <- ggplot(log2(rna_k4me3_data), aes(x=ILC2_RNA_avg, y=ILC2_K
 ggsave(rna_k4me3_data_plot, file = paste0(out.dir, "Bruce_RNA_Laurie_H3K4me3_correlation", ".pdf"),
        height = 6, width = 6 , device = "pdf")
 
+# Save Session Info ------------------------------------------------------------
+
+writeLines(capture.output(sessionInfo()), "./sessionInfo/Figure_1b_sessionInfo.txt")
+
