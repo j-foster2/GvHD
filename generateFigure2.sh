b3c9fd53d745eb0aef2d612e49aba581d9d595f3
Joseph Foster <jfoster3@longleaf-login4.its.unc.edu>
2023-08-28 14:55:15 -0400

    Generates all figures for manuscript.

--- generateFigure2.sh
+++
@@ -1,3 +0,0 @@
-ssub --mem=100g --time=0-2 -N 1 -n 20 --wrap=\"apptainer exec --no-mount home --bind $PWD ../Test_Environment/gvhd.sif Rscript integrate_multiome_replicate2.R\"
-ssub --mem=10g --time=0-0:45 -N 1 -n 1 --wrap=\"apptainer exec --no-mount home --bind $PWD ../Test_Environment/gvhd.sif Rscript Figure_2b.R\"
-ssub --mem=10g --time=0-0:45 -N 1 -n 1 -o ./slurmOut/ExtData_Figure_2g_j --wrap=\"apptainer exec --no-mount home --bind $PWD ../Test_Environment/gvhd.sif Rscript ExtData_Figure_2g_j.R\"
